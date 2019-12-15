45e3804aeb76d3f11d15ee17f58730926cfaba63
irma1991 <irmalinartaite@gmail.com>
2019-12-15 23:22:34 +0200

    nothing new

--- js/app.js
+++ js/app.js
@@ -1,31 +1,27 @@
-// function today(value){
-//     return  value.forecastTimeUtc.includes('2019-12-13');
-// }
-
 const fetchWeather = async () => {
-   weather = await fetch(
+    weather = await fetch(
         'https://api.meteo.lt/v1/places/kaunas/forecasts/long-term'
     ).then(res => res.json());
 };
+
+
+// filtruojam duomenys tik konkrecios dienos
+
+function today(value) {
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+    var formedDate = year + "-" + month + "-" + day;
+    console.log(formedDate)
+    return value.forecastTimeUtc.includes(formedDate);
+}
+
 const showWeather = async () => {
 // getting the weather data from api
     await fetchWeather();
 
-    // filtruojam duomenys tik konkrecios dienos
-
-    function today(value){
-        var currentDate = new Date();
-        var day = currentDate.getDate();
-        var month = currentDate.getMonth() + 1;
-        var year = currentDate.getFullYear();
-        var formated = year+"-"+month+"-"+day;
-        return value.forecastTimeUtc.includes(formated);
-    }
-
-    let weatherItems = weather.forecastTimestamps;
-    weatherItems = weatherItems.filter(today)
-
-    //susikuriu icons kieknievam galimam conditionCode
+    //susikuriu icons kiekvienam galimam conditionCode
     let weatherIcons = {
         clear: '<i class="fas fa-sun"></i>',
         isolatedClouds: '<i class="fas fa-cloud"></i>',
@@ -42,7 +38,7 @@ const showWeather = async () => {
         humidityIcon: '<i class="fas fa-tint"></i>'
     };
 
-    async function getWeatherIcon(conditionCode){
+    async function getWeatherIcon(conditionCode) {
         switch (conditionCode) {
             case ("clear"):
                 return weatherIcons.clear;
@@ -71,63 +67,53 @@ const showWeather = async () => {
         }
     }
 
+    let weatherItems = weather.forecastTimestamps;
+    weatherItems = weatherItems.filter(today)
 
-    for(let i=0; i < 7; i++){
+    const maxtemp = Math.max(...weatherItems.map(o => o.airTemperature));
+    console.log(maxtemp)
 
-        const weatherByDays = document.createElement('div');
-        weatherByDays.classList.add("col-sm", "border");
-        document.querySelector('.weather-by-days').appendChild(weatherByDays);
+    const mintemp = Math.min(...weatherItems.map(o => o.airTemperature));
+    console.log(mintemp)
 
-        let particularDay = document.createElement('div');
-        var str = weatherItems[i]['forecastTimeUtc'];
-        var res = str.slice(0, 10);
-        particularDay.classList.add("particular-day");
-        particularDay.textContent = res;
-        weatherByDays.appendChild(particularDay);
+        for (let i = 0; i < weatherItems.length; i++) {
 
-        let weatherIconDay = document.createElement('div');
-        weatherIconDay.classList.add("weather-icon-day");
-        weatherIconDay.innerHTML = await getWeatherIcon(weatherItems[i]['conditionCode']);
-        weatherByDays.appendChild(weatherIconDay);
+            const weatherByHours = document.createElement('div');
+            weatherByHours.classList.add("col-sm", "border");
+            document.querySelector('.weather-by-hours').appendChild(weatherByHours);
+
+            let hours = document.createElement('div');
+            var str = weatherItems[i]['forecastTimeUtc'];
+            var res = str.slice(-8, -3);
+            hours.classList.add("hours");
+            hours.textContent = res;
+            weatherByHours.appendChild(hours);
+
+            let weatherIcon = document.createElement('div');
+            weatherIcon.classList.add("weather-icon");
+            weatherIcon.innerHTML = await getWeatherIcon(weatherItems[i]['conditionCode']);
+            weatherByHours.appendChild(weatherIcon);
+
+            let temperature = document.createElement('div');
+            temperature.classList.add("temperature");
+            temperature.textContent = weatherItems[i]['airTemperature'] + " °";
+            weatherByHours.appendChild(temperature);
+
+            let humidityIcon = document.createElement('div');
+            humidityIcon.classList.add("humidity-icon");
+            humidityIcon.innerHTML = weatherIcons.humidityIcon;
+            weatherByHours.appendChild(humidityIcon);
+
+            let humidityValue = document.createElement('div');
+            humidityValue.classList.add("humidity-value");
+            humidityValue.textContent = weatherItems[i]['totalPrecipitation'] + "%";
+            weatherByHours.appendChild(humidityValue);
+
+            let wind = document.createElement('div');
+            wind.classList.add("wind");
+            wind.textContent = weatherItems[i]['windSpeed'] + "m/s";
+            weatherByHours.appendChild(wind);
+        }
     }
 
-    for(let i=0; i < weatherItems.length; i++){
-
-        const weatherByHours = document.createElement('div');
-        weatherByHours.classList.add("col-sm", "border");
-        document.querySelector('.weather-by-hours').appendChild(weatherByHours);
-
-        let hours = document.createElement('div');
-        var str = weatherItems[i]['forecastTimeUtc'];
-        var res = str.slice(-8, -3);
-        hours.classList.add("hours");
-        hours.textContent = res;
-        weatherByHours.appendChild(hours);
-
-        let weatherIcon = document.createElement('div');
-        weatherIcon.classList.add("weather-icon");
-        weatherIcon.innerHTML = await getWeatherIcon(weatherItems[i]['conditionCode']);
-        weatherByHours.appendChild(weatherIcon);
-
-        let temperature = document.createElement('div');
-        temperature.classList.add("temperature");
-        temperature.textContent = weatherItems[i]['airTemperature']+" °";
-        weatherByHours.appendChild(temperature);
-
-        let humidityIcon = document.createElement('div');
-        humidityIcon.classList.add("humidity-icon");
-        humidityIcon.innerHTML = weatherIcons.humidityIcon;
-        weatherByHours.appendChild(humidityIcon);
-
-        let humidityValue = document.createElement('div');
-        humidityValue.classList.add("humidity-value");
-        humidityValue.textContent = weatherItems[i]['totalPrecipitation']+"%";
-        weatherByHours.appendChild(humidityValue);
-
-        let wind = document.createElement('div');
-        wind.classList.add("wind");
-        wind.textContent = weatherItems[i]['windSpeed']+"m/s";
-        weatherByHours.appendChild(wind);
-    }
-}
 showWeather()
